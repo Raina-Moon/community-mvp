20109d473d0c41f45aec15e6ca8b4338e4b8979f
Raina Moon <mds6425@gmail.com>
2025-08-27 19:35:04 +0900

    feat : 홈페이지에 포스팅 리스트 데이터 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, Text, View } from "react-native";
 import PostCard from "../src/components/PostCard";
+import { usePostsQuery } from "@/src/hooks/usePosts";
 
 const Home = () => {
+  const { data } = usePostsQuery();
+
   return (
     <View>
       <Text>Home Screen</Text>
-       <FlatList
+      <FlatList
+        data={data}
         keyExtractor={(p) => p.id}
         renderItem={({ item }) => <PostCard post={item} />}
       />
