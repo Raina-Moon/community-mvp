bd53ec6d74b963bb02f86ecf32f78bbf3a5838aa
Raina Moon <mds6425@gmail.com>
2025-08-27 17:00:19 +0900

    feat : 홈페이지 생성 및 포스트카드 컴포넌트 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import PostCard from "../src/components/PostCard";
 
 const Home = () => {
   return (
     <View>
       <Text>Home Screen</Text>
+       <FlatList
+        keyExtractor={(p) => p.id}
+        renderItem={({ item }) => <PostCard post={item} />}
+      />
     </View>
   );
 };
